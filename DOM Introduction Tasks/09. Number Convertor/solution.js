15826b7c14efbfad35739ccc137fe61e5515cba5
vladimirSv <varbanov910@gmail.com>
2022-06-26 00:01:20 +0300

    update Task 9 - DOM Introduction_ Number Converter

--- DOM Introduction Tasks/09. Number Convertor/solution.js
+++ DOM Introduction Tasks/09. Number Convertor/solution.js
@@ -1,4 +1,33 @@
 function solve() {
+  
+  
+    let optionBinary = document.createElement('option');
+    optionBinary.value = 'binary';
+    optionBinary.innerHTML = 'Binary';
+    let optionHexadecimal = document.createElement('option');
+    optionHexadecimal.value = 'hexadecimal';
+    optionHexadecimal.innerHTML = 'Hexadecimal';
 
-    //TODO...
+    let selectedElementsToSwitch = document.getElementById('selectMenuTo');
+    selectedElementsToSwitch.appendChild(optionBinary);
+    selectedElementsToSwitch.appendChild(optionHexadecimal);
+    
+    let converted = 0;
+    // let decimaInputNumber = Number(document.getElementById('input').value);
+    document.getElementsByTagName('button')[0].addEventListener("click", convertFunction);
+   
+    function convertFunction(){
+        let decimaInputNumber = Number(document.getElementById('input').value);
+        let convertTo = selectedElementsToSwitch.value;
+        if( convertTo == 'binary' ){
+            converted = decimaInputNumber.toString(2);
+            resultElement = document.getElementById('result');
+            resultElement.value = converted;
+        } else if (convertTo == 'hexadecimal' ){
+            converted = decimaInputNumber.toString(16).toUpperCase();
+            resultElement = document.getElementById('result');
+            resultElement.value = converted;
+        }
+    }
+     
 }
